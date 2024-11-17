8177a9df6580e6c897e1646a981af063e1c9354d
Radosław Sienkiewicz <rsienkiewicz88@gmail.com>
2024-11-17 22:09:15 +0100

    licenses

--- lib/src/profile/presentation/pages/profile_page.dart
+++ lib/src/profile/presentation/pages/profile_page.dart
@@ -1,11 +1,19 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flow_zero_waste/config/assets/color/app_colors.dart';
+import 'package:flow_zero_waste/config/gen/assets.gen.dart';
+import 'package:flow_zero_waste/config/injection/injection.dart';
 import 'package:flow_zero_waste/config/routes/navigation_router.gr.dart';
 import 'package:flow_zero_waste/core/common/presentation/logics/providers/responsive_ui/page_provider.dart';
 import 'package:flow_zero_waste/core/common/presentation/widgets/components/nav_bar.dart';
 import 'package:flow_zero_waste/core/extensions/l10n_extension.dart';
+import 'package:flow_zero_waste/core/extensions/theme_extension.dart';
+import 'package:flow_zero_waste/core/services/device_info/device_info_manager.dart';
 import 'package:flow_zero_waste/src/auth/presentation/logics/auth_provider.dart';
+import 'package:flow_zero_waste/src/profile/presentation/widgets/licenses_widget.dart'
+    as licenses;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 @RoutePage()
 class ProfilePage extends StatelessWidget {
@@ -60,28 +68,64 @@ class ProfilePage extends StatelessWidget {
             ProfileOption(
               icon: Icons.article,
               title: translations.profileBlog,
+              onTap: () =>
+                  launchUrl(Uri.parse('https://flowzerowaste.com/blog')),
             ),
             ProfileOption(
               icon: Icons.store,
               title: translations.profileRegisterLocal,
+              onTap: () => launchUrl(
+                  Uri.parse('https://flowzerowaste.com/register-local')),
             ),
             ProfileOption(
               icon: Icons.recommend,
               title: translations.profileRecommendPlace,
+              onTap: () =>
+                  launchUrl(Uri.parse('https://flowzerowaste.com/recommend')),
             ),
             const SizedBox(height: 24),
             SectionTitle(title: translations.profileSupport),
             ProfileOption(
               icon: Icons.help,
               title: translations.profileHelp,
+              onTap: () =>
+                  launchUrl(Uri.parse('https://flowzerowaste.com/help')),
             ),
             ProfileOption(
               icon: Icons.info,
               title: translations.profileHowItWorks,
+              onTap: () => launchUrl(
+                  Uri.parse('https://flowzerowaste.com/how-it-works')),
             ),
             ProfileOption(
               icon: Icons.gavel,
               title: translations.profileLegal,
+              onTap: () =>
+                  launchUrl(Uri.parse('https://flowzerowaste.com/legal')),
+            ),
+            ProfileOption(
+              icon: Icons.article,
+              title: translations.profileLicenses,
+              onTap: () async {
+                final packageInfo = await locator<DeviceInfoManager>().package;
+                if (!context.mounted) return;
+                licenses.showLicensePage(
+                  context: context,
+                  title: Text(
+                    translations.profileLicenses,
+                    style: context.textTheme.headlineSmall,
+                  ),
+                  applicationIcon: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      Assets.icons.flowLogoProduction.path,
+                    ),
+                  ),
+                  applicationLegalese:
+                      '© ${DateTime.now().year} Radosław Sienkiewicz',
+                  applicationName: packageInfo.appName,
+                  applicationVersion: packageInfo.version,
+                );
+              },
             ),
           ],
         ),
